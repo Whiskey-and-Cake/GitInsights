c191ab3bb4e48ac100eb8f3590b623df1ce0b78d
Kate Jefferson <barbedbella@gmail.com>
2015-03-27 14:18:16 -0500

    (fix) injector error. refactoring.

--- server/firebaseTest.js
+++ server/firebaseTest.js
@@ -1,37 +1,64 @@
-/**
- * Firebase module
- * @type {module|*}
- */
-var db = angular.module('Firebase', ['firebase']);
+var db = angular.module('FirebaseApp', ['firebase']);
 // makes $firebaseObject, $firebaseArray, and $firebaseAuth available
 
-/** Favorite Factory
- * returns the user's list of favorites
- */
-db.factory('favoritesList', ['$firebaseObject',
-  function($firebaseObject) {
-    return function(username) {
-      // need reference to user's data ?
-      var ref = new Firebase('https://gitinsights.firebaseio.com/');
-      var profileRef = ref.child(username);
-      // return synchronized object
-      return $firebaseObject(profileRef);
-    };
+//db.factory('favoritesList', ['$firebaseObject',
+//  function($firebaseObject) {
+//    return function(username) {
+//      // need reference to user's data ?
+//      var ref = new Firebase('https://gitinsights.firebaseio.com/');
+//      return $firebaseObject(ref);
+//    };
+//  }
+//]);
+
+db.factory('FireFactory', function($scope, $firebaseAuth, $firebaseObject) {
+  var ref = new Firebase('https://gitinsights.firebaseio.com');
+  return {
+    auth: $firebaseAuth(ref),
+    favorites: $firebaseObject(ref)
+  };
+});
+
+db.controller('FavoriteController', ['$scope', 'FireFactory', '$firebaseObject',
+  function($scope, FireFactory) {
+    //var ref = new Firebase('https://gitinsights.firebaseio.com/');
+    var obj = FireFactory.favorites;
+
+    obj.$loaded().then(function() {
+      // accesses each record
+      angular.forEach(obj, function(value, key) {
+        console.log(key,value);
+      })
+    });
+
+    // make object available to scope
+    $scope.data = obj;
+
+    // three-way binding
+    obj.$bindTo($scope, 'data');
+
+    function addFavorite(favorite) {
+      obj.favorite = favorite;
+      obj.user = authData.uid;
+      obj.$save().then(function(ref) {
+        obj.key() === obj.$id;
+      }, function(error) {
+        console.log('Error:', error);
+      });
+    }
+
   }
 ]);
 
-/**
- * Firebase controller for favorite list
- */
-db.controller('favoriteController', ['$scope', 'favoritesList',
-  function($scope, favoritesList) {
-    // make list available to DOM
-    // need username from auth
-    $scope.user = 'Guest' + Math.round(Math.random() * 100);
-    $scope.list = favoritesList($scope.user);
+db.controller('GitAuth', ['FireFactory', '$firebaseAuth', function($scope, FireFactory) {
+  var auth = FireFactory.auth;
+  auth.$authWithOAuthPopup('github', function(error, authData) {
+    if (error) {
+      console.log('Login failed:', error);
+    } else {
+      console.log('Authenticated successfully with payload:', authData);
+    }
+  });
 
-    // 3-way binding, may eliminate need for save function
-    favoritesList($scope.user).$bindTo($scope, 'favoritesList');
-  }
-]);
+}]);
 
